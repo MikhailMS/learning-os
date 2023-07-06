959de8610c2b96b724b4f6aaa44ea22f612481e2
MikhailMS <1488maiklm@gmail.com>
2023-07-06 23:48:09 +0100

    Add macros output for UART

--- src/macros.rs
+++ src/macros.rs
@@ -8,3 +8,16 @@ macro_rules! println {
     ()            => ($crate::print!("\n"));
     ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
 }
+
+#[macro_export]
+macro_rules! serial_print {
+    ($($arg:tt)*) => ($crate::serial_uart::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! serial_println {
+    ()                       => ($crate::serial_print!("\n"));
+    ($fmt:expr)              => ($crate::serial_print!(concat!($fmt, "\n")));
+    ($fmt:expr,$($arg:tt)* ) => ($crate::serial_print!(concat!($fmt, "\n"), $($arg)*));
+}
+
