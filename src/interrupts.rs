ca682af04b412a42acba30fc0d80715916af1283
MikhailMS <1488maiklm@gmail.com>
2023-07-11 23:56:08 +0100

    Add support for keyboard interrupts

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,7 +1,11 @@
 use lazy_static::lazy_static;
+use pc_keyboard::{ layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1 };
 use pic8259::ChainedPics;
-use spin;
-use x86_64::structures::idt::{ InterruptDescriptorTable, InterruptStackFrame };
+use spin::Mutex;
+use x86_64::{
+    instructions::port::Port,
+    structures::idt::{ InterruptDescriptorTable, InterruptStackFrame }
+};
 
 use crate::{ print, println };
 use crate::gdt;
@@ -9,7 +13,7 @@ use crate::gdt;
 pub const PIC1_OFFSET: u8 = 32;
 pub const PIC2_OFFSET: u8 = PIC1_OFFSET + 8;
 
-pub static PICS: spin::Mutex<ChainedPics> = spin::Mutex::new(unsafe {
+pub static PICS: Mutex<ChainedPics> = Mutex::new(unsafe {
     ChainedPics::new(PIC1_OFFSET, PIC2_OFFSET)
 });
 
@@ -17,6 +21,21 @@ pub static PICS: spin::Mutex<ChainedPics> = spin::Mutex::new(unsafe {
 #[repr(u8)]
 pub enum InterruptIndex {
     Timer = PIC1_OFFSET,
+    Keyboard,
+    SecondaryIC,
+    SerialPort2,
+    SerialPort1,
+    ParallelPort23,
+    FloppyDisk,
+    ParallelPort1,
+    RealTimeClock,
+    ACPI,
+    Available1,
+    Available2,
+    Mouse,
+    CoProcessor,
+    PrimaryAta,
+    SecondaryAta
 }
 
 impl InterruptIndex {
@@ -42,9 +61,19 @@ lazy_static! {
 
         idt[InterruptIndex::Timer.as_usize()]
             .set_handler_fn(timer_interrupt_handler);
+        idt[InterruptIndex::Keyboard.as_usize()]
+            .set_handler_fn(keyboard_interrupt_handler);
 
         idt
     };
+}
+
+lazy_static! {
+    static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> = Mutex::new(
+        // HandleControl allows to add support for Unicode characters [U+0001; U+001A], however it
+        // is not required at the moment
+        Keyboard::new(layouts::Us104Key, ScancodeSet1, HandleControl::Ignore)
+    );
 }
 
 pub fn init_idt() {
@@ -66,6 +95,27 @@ extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFr
     unsafe {
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    }
+}
+
+extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    let mut keyboard = KEYBOARD.lock();
+    let mut ps2_port = Port::new(0x60); // 0x0060-0x0064: The "8042" PS/2 Controller or its predecessors, dealing with keyboards and mice
+
+    let scancode: u8 = unsafe { ps2_port.read() };
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}",   character),
+                DecodedKey::RawKey(key)        => print!("{:?}", key),
+            }
+        }
+    }
+
+    // Unsafe because notify_end_of_interrupt() can potentially cancel unsent interrupt or cause system to hang
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
 }
 
