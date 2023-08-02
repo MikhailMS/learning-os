36e332801b54e2ffe92069c8dc6b3e800d7c5da8
MikhailMS <1488maiklm@gmail.com>
2023-08-02 23:36:40 +0100

    Add keyboard task to main so we can use keyboard when running Kernel

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,18 @@ extern crate alloc;
 use bootloader::{ BootInfo, entry_point };
 use core::panic::PanicInfo;
 use radius_os::{
+    memory::{ self, BootInfoFrameAllocator },
+    task::{
+        executor::Executor,
+        keyboard::print_keypress,
+        Task,
+    },
+    allocator,
     hlt_loop,
     println,
     vga,
 };
+use x86_64::VirtAddr;
 
 #[cfg(test)]
 use radius_os::test_panic_handler;
@@ -37,13 +45,30 @@ fn panic(info: &PanicInfo) -> ! {
 
 entry_point!(kernel_main);
 
-fn kernel_main(_boot_info: &'static BootInfo) -> ! {
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     radius_os::init(); // Initialise Interrupt Descriptor Table for our kernel
 
     vga::WRITER.lock().write_string("Hello there!");
     vga::WRITER.lock().write_byte(b'H');
     vga::WRITER.lock().write_byte_at(b'L', 10, 40);
+
+    // Initialise Heap space >>>>>>>
+    let phys_memory_offset  = VirtAddr::new(boot_info.physical_memory_offset);
+    // Initialise mapper (virt to phys)
+    let mut mapper          = unsafe { memory::init(phys_memory_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialisation failed");
+    // <<<<<<<<
     
+    #[cfg(test)]
+    test_main();
+
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(print_keypress()));
+    executor.run();
+
     println!("It works!");
 
     // x86_64::instructions::interrupts::int3(); // Invoke breakpoint exception
@@ -51,9 +76,6 @@ fn kernel_main(_boot_info: &'static BootInfo) -> ! {
     // Let's cause page fault
     // let ptr = 0x2057f3 as *mut u8;
     // unsafe { *ptr = 42; }
-
-    #[cfg(test)]
-    test_main();
 
     hlt_loop();
 }
